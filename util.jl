f472afbd955e95e83e84e36344b66dbb1e9d30e5
wmkouw <wmkouw@gmail.com>
2020-07-23 21:30:04 +0200

    Added notebook with CIFAR-10 data

--- util.jl
+++ util.jl
@@ -3,17 +3,51 @@
 using Flux: chunk
 
 
-function convert_to_image(x, y_size)
+function convert_to_image(X::Array{Float32,2}, num_chunks; rgb=false, patch_size=28)
     "Reshape a mini-batch to an image collage"
 
+    # Check mini-batch size
+    batch_size = size(X,2)
+
     # Map to [0,1] interval
-    x = sigmoid.(x)
+    X = sigmoid.(X)
 
-    # Cut to chunks
-    chunks = chunk(x, y_size)
+    if rgb
 
-    # Reshape
-    img = permutedims(vcat(reshape.(chunks, 28, :)...), (2, 1))
+        # Patch size
+        imsz = patch_size*patch_size
 
-    return img
+        # Split by color channel
+        XR = X[0*imsz+1:1*imsz,:]
+        XG = X[1*imsz+1:2*imsz,:]
+        XB = X[2*imsz+1:3*imsz,:]
+
+        # Cut to chunks
+        chunksR = chunk(XR, num_chunks)
+        chunksG = chunk(XG, num_chunks)
+        chunksB = chunk(XB, num_chunks)
+
+        # Reshape into images
+        imagesR = vcat(reshape.(chunksR, patch_size, :)...)
+        imagesG = vcat(reshape.(chunksG, patch_size, :)...)
+        imagesB = vcat(reshape.(chunksB, patch_size, :)...)
+
+        # Join channels
+        images = cat(imagesR, imagesG, imagesB, dims=3)
+
+        # Transpose images
+        return permutedims(images, (2, 1, 3))
+
+    else
+
+        # Cut to chunks
+        chunks = chunk(X, num_chunks)
+
+        # Reshape into images
+        images = vcat(reshape.(chunks, patch_size, :)...)
+
+        # Transpose images
+        return permutedims(images, (2, 1))
+
+    end
 end
